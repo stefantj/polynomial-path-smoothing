2d368636233c131d4d5a2f7bc7b88c6f2a01b580
Trevor Murphy <trevor05b@gmail.com>
2016-10-28 09:00:02 -0700

    a save before reinsituting jump

--- BestLocalPlannerGuts.jl
+++ BestLocalPlannerGuts.jl
@@ -705,19 +705,7 @@ function costFunc(dF, sol::PathSol, prob::PathProblem, solvStuff::PolyPathSolver
     
     #Create costmap costs
     #Note that the mapCost with a cell travel cost of 50 is on the order of 1000s
-    mapCost = 0;
-    for  i in timesCheck
-        if(prob.isDim3)
-            mapCost += prob.costmap[round(Int64,evaluate_poly(p[:,1],0,1)/prob.grid_resolution),
-                    round(Int64,evaluate_poly(p[:,2],0,i)/prob.grid_resolution),
-                    round(Int64,evaluate_poly(p[:,3],0,i)/prob.grid_resolution)];
-        else
-            mapCost += prob.costmap[round(Int64,evaluate_poly(p[:,1],0,1)/prob.grid_resolution),
-                round(Int64,ceil(evaluate_poly(p[:,2],0,i)/prob.grid_resolution)+1),
-                    1];
-        end
-    end
-    mapCost *= tuning.obstacleWeight;
+    mapCost = tuning.obstacleWeight*sum(prob.costmap[sol.cells]);
 
     #Return the total cost
     cost = derivCost + velCost + softCosts + mapCost+accelCost;
